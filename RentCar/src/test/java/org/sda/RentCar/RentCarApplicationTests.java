420b0efbffde7a185c5f21f981972dfc619a76d5
Urim Toshi <urimtoshi2@gmail.com>
2024-01-21 10:29:09 +0100

    initial commit

---
+++ RentCar/src/test/java/org/sda/RentCar/RentCarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sda.RentCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
