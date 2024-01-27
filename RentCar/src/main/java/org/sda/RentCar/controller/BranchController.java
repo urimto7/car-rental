c9d2be7a9f33bf2fec69b1b9ec610d5b51d7b9f5
fitim <fitim.hoxha369@gmail.com>
2024-01-27 14:53:50 +0100

    implemented CRUD for branch

--- RentCar/src/main/java/org/sda/RentCar/controller/BranchController.java
+++ RentCar/src/main/java/org/sda/RentCar/controller/BranchController.java
@@ -1,4 +1,45 @@
 package org.sda.RentCar.controller;
 
+import org.sda.RentCar.dto.BranchDTO;
+import org.sda.RentCar.service.BranchService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/branch")
+
+
 public class BranchController {
+
+    @Autowired
+    private BranchService branchService;
+
+    @PostMapping
+    public void add(@RequestBody BranchDTO branchDTO) {
+        branchService.add(branchDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BranchDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok(branchService.findById(id));
+    }
+
+    @PutMapping
+    public void update(@RequestBody BranchDTO branchDTO) {
+        branchService.update(branchDTO);}
+
+    @DeleteMapping
+    public void delete(@PathVariable Integer id) {
+        branchService.delete (id);}
+
+    @GetMapping
+    public ResponseEntity<List<BranchDTO>> findAll() {
+        return ResponseEntity.ok(branchService.findAll());}
+
+
+
+
 }
--- RentCar/src/main/java/org/sda/RentCar/repository/BranchRepository.java
+++ RentCar/src/main/java/org/sda/RentCar/repository/BranchRepository.java
@@ -1,4 +1,10 @@
 package org.sda.RentCar.repository;
 
-public interface BranchRepository {
+import org.sda.RentCar.model.Branch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BranchRepository extends JpaRepository<Branch, Integer> {
+
 }
