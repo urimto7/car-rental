da50ab55e992eb322f02481e5377c8bbf90814da
Marsela2023 <cekrezi.prof@hotmail.com>
2024-01-27 14:27:37 +0100

    implemented CRUD for reservations

---
+++ RentCar/src/main/java/org/sda/RentCar/model/Reservations.java
@@ -0,0 +1,32 @@
+package org.sda.RentCar.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "reservation")
+public class Reservations {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_reservation")
+    private int id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "pickup_date")
+    private String pickupDate;
+    @Column(name = "days")
+    private int days;
+    @Column(name = "status")
+    private String status;
+    @OneToMany
+    @JoinColumn (name = "id_user", referencedColumnName = "id")
+    private User user;
+    @OneToMany
+    @JoinColumn(name = "id_car", referencedColumnName = "id")
+    private Car car;
+
+}
+
